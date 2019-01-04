61e9b4b0b4983d8f90a311074f5f9317f29995d2
Sukwon Choi <swchoi06@snatcher.ai>
2019-01-04 04:30:34 +0000

    Update script to install zsh and oh-my-zsh automatically

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -5,9 +5,16 @@ CURRENT_DIR=$PWD
 BASE_DIR=$(dirname $0)
 
 cd $BASE_DIR
+# Install zsh
+sudo apt-get install zsh
+chsh -s `which zsh`
+
 # Install zgen
 rm -rf ~/.zgen
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+
+# Install ohmyzsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # Install ohmyzsh custom plugin
 rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
