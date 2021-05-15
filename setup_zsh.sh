56c9b3e9d932dedfddfc9db8634ed4125ee1efe9
nevergreendd <nevergreendd@gmail.com>
2021-05-15 17:41:55 +0900

    Some ommands not in use removed

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -6,12 +6,12 @@ BASE_DIR=$(dirname $0)
 
 cd $BASE_DIR
 # Install zsh
-sudo apt-get install zsh
-chsh -s `which zsh`
+#sudo apt-get install zsh
+#chsh -s `which zsh`
 
 # Install zgen
-rm -rf ~/.zgen
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+#rm -rf ~/.zgen
+#git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
 # Install ohmyzsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
@@ -21,9 +21,9 @@ rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Install Virtualenv
-sudo apt-get install python3-pip python3-dev
-sudo pip3 install virtualenv virtualenvwrapper
-mkdir .virtualenvs
+#sudo apt-get install python3-pip python3-dev
+#sudo pip3 install virtualenv virtualenvwrapper
+#mkdir .virtualenvs
 
 # Replace zshrc
 rm -rf ~/.zshrc
